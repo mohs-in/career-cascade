9bbde6a6071ad32e4fc9f495685ba79f5d46a379
Mohsin <in.mohsin@outlook.com>
2024-07-04 11:13:19 +0530

    connecting frontend and backend

---
+++ backend/src/main/java/com/spring/bootrest/controller/JobRestController.java
@@ -0,0 +1,58 @@
+package com.spring.bootrest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.spring.bootrest.model.JobPost;
+import com.spring.bootrest.service.JobService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000" )
+public class JobRestController {
+	
+	@Autowired
+	private JobService service;
+
+	@GetMapping("jobPosts")
+	public List<JobPost> getAllJobs() {
+		return service.getAllJobs();
+	}
+
+	@GetMapping("jobPost/{postId}")
+	public JobPost getJob(@PathVariable int postId) {
+		return service.getJob(postId);
+	}
+
+	@PostMapping("jobPost")
+	public JobPost addJobPost(@RequestBody JobPost jobPost) {
+		service.addJobPost(jobPost);
+		return service.getJob(jobPost.getPostId());
+	}
+
+	@PutMapping("jobPost")
+	public JobPost updateJob(@RequestBody JobPost jobPost) {
+		return service.updateJob(jobPost);
+	}
+
+	@DeleteMapping("jobPost/{postId}")
+	public String deleteJob(@PathVariable int postId) {
+
+		service.deleteJob(postId);
+		return "Deleted";
+
+	}
+
+	@GetMapping("load")
+	public String loadData() {
+		service.load();
+		return "success";
+	}
+
+	@GetMapping("jobPosts/keyword/{keyword}")
+	public List<JobPost> searchByKeyword(@PathVariable String keyword) {
+		return service.search(keyword);
+	}
+
+}
---
+++ backend/src/main/java/com/spring/bootrest/repo/JobRepo.java
@@ -0,0 +1,15 @@
+package com.spring.bootrest.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.bootrest.model.JobPost;
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer> {
+
+    List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+
+}
